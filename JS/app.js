1faf285fc27b772b8beeb3ea7590b90de76c4892
Kamaljit Bhullar Desktop <kamaljitbhuller@gmail.com>
2021-04-03 16:48:13 -0700

    added JS folder and app.js along with functions to plan for part 2.

---
+++ JS/app.js
@@ -0,0 +1,39 @@
+// toggle dark  mode. 
+// using fuctions assign element to certain part of the document
+// use toggle to change the color to dark-mode and vice versa. on-click
+
+/*function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }*/
+
+
+//created a function to create a text area when '+' is clicked
+// create a varible name txt-area and use createelement for next text area. 
+// created other btn save and delete for each txt_area to save and delete. 
+
+
+/*function noteFunction() {
+   var txt_area = document.createElement("textarea");
+   txt_area.rows = 10;
+   txt_area.cols = 50;
+   var btn = document.createElement('button')
+   var btn2 = document.createElement('button')
+   btn.innerHTML= 'Save';
+   btn2.innerHTML= 'Cancel';
+   document.querySelector('.main-part').after(txt_area, btn, btn2);
+}*/
+
+
+// delete the text area. 
+
+// function delete_txtarea(){
+    //do smth and clear/reset the text area and delete current target text area. 
+//}
+
+
+// saveing the text
+
+//funciton save_text(){
+    // copy the text and save it to file MAYBE. and paste a link to sidebar. ??
+//}
